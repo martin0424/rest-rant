8bca100426ff9ed46348fdbb056b2301a461bb7f
martin0424 <h.newland424@gmail.com>
2022-06-13 17:38:07 -0400

    added eslint and places controllers

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
+
 dotenv.config();
 const app = express();
 
-app.get("/", (_req, res) => {
-  res.send("Hello world!");
+app.get('/', (_req, res) => {
+  res.send('Hello world!');
 });
 
-app.get("*", (_req, res) => {
-  res.status(404).send("404 you suck!");
+app.get('*', (_req, res) => {
+  res.status(404).send('404 you suck!');
 });
 
 app.listen(process.env.PORT, () => {
