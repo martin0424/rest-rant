80f24c3e236a085fed99716b23b555265ee9ee72
martin0424 <h.newland424@gmail.com>
2022-06-30 16:39:06 -0400

    updated form data

--- controllers/places.js
+++ controllers/places.js
@@ -4,6 +4,11 @@ router.get('/new', (req, res) => {
   res.render('places/new');
 });
 
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('POST /places');
+});
+
 router.get('/', (req, res) => {
   res.render('places/index');
 });
