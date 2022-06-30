683ec74546143dce5ec026904c30eaa0332a3396
martin0424 <h.newland424@gmail.com>
2022-06-30 17:32:53 -0400

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -27,4 +27,17 @@ router.get('/', (req, res) => {
   res.render('places/index');
 });
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render('error404');
+  }
+  else if (!places[id]) {
+    res.render('error404');
+  }
+  else {
+    res.render('places/show', { places: places[id] });
+  }
+});
+
 module.exports = router;
